5b315eeaa592fbd545c1d818cf41830fc390fae9
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-10-20 16:29:19 -0300

    Add themes

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -4,14 +4,29 @@ import Blank from '../screens/Blank';
 import Login from '../screens/Login';
 import Details from '../screens/Details';
 import HomeTab from './HomeTab';
+import theme from '../styles/theme.style';
 
 const RootStack = createStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <RootStack.Navigator>
+    <RootStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.PRIMARY_COLOR,
+        },
+        headerTintColor: theme.TEXT_LIGHT,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
       <RootStack.Screen name="Login" component={Login} />
-      <RootStack.Screen name="Home" component={HomeTab} />
+      <RootStack.Screen
+        name="Home"
+        component={HomeTab}
+        options={{ headerTitle: undefined }}
+      />
       <RootStack.Screen name="Details" component={Details} />
     </RootStack.Navigator>
   );
